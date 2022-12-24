026fe409c82b6483f1e08f8b8d1cd1318403cf09
Bharat Ranjan <bhaarat.ranjan@gmail.com>
2022-12-24 21:43:33 +0530

    First commit

---
+++ src/Game.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Board from "./Compnents/Board";
+
+const Game = () => {
+    return (
+        <div>
+            <h1 className="text">Tic-Tac-Toe</h1>
+            <Board />
+        </div>
+    )
+}
+
+export default Game;
